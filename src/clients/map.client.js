01c56dbd0a300a1cbcc53ef45bc75df4c9eff1f0
minh1622 <huytranngoc098@gmail.com>
2021-08-09 22:24:12 +0700

    show data sensor

--- src/clients/map.client.js
+++ src/clients/map.client.js
@@ -1,40 +1,38 @@
-module.exports = function() {
+const subscribe_data = require('./subscribe_data.client')
+
+module.exports = function () {
     // Location
-    window.lat = 21.0066095;
+    window.lat = 21.0065064;
     window.lng = 105.8431323;
     var map;
-    // var mark;
-    // var lineCoords = [];
-    // var markers = [];
-    // var newnum;
     const sensorGPS = [
-        {lat: 21.0066095, lng: 105.8431323},
-        {lat: 21.0065419, lng: 105.8432735},
-        {lat: 21.0064809, lng: 105.8431522},
-        {lat: 21.0065144, lng: 105.8429997}
+        { lat: 21.0066095, lng: 105.8431323 },
+        { lat: 21.0065419, lng: 105.8432735 },
+        { lat: 21.0064809, lng: 105.8431522 },
+        { lat: 21.0065144, lng: 105.8429997 }
     ];
 
     //create map
     // New map
-    var initialize = function() {
+    var initialize = function () {
         map = new google.maps.Map(document.getElementById('map-canvas'), {
-            center:{lat:lat,lng:lng},
-            zoom:18
+            center: { lat: lat, lng: lng },
+            zoom: 18
         });
+
         const image = {
             url: "/static/image/drone.png"
         };
-        mark = new google.maps.Marker({
-            position:{lat:lat, lng:lng}, 
-            map:map,
+        var mark = new google.maps.Marker({
+            position: { lat: lat, lng: lng },
+            map: map,
             icon: image,
             shouldFocus: false
         });
-        for (let i = 0; i<sensorGPS.length; i++){
+        for (let i = 0; i < sensorGPS.length; i++) {
             newSensor(sensorGPS[i], i)
         }
     };
-    window.initialize = initialize;
 
     //Set new marker sensor
     function newSensor(position, num) {
@@ -43,25 +41,43 @@ module.exports = function() {
             map,
         })
         const contentString = '<div id="content">' +
-        '<div id="siteNotice">' +
-        "</div>" +
-        `<h4 id="firstHeading">Sensor ${num}</h4>` +
-        '<div id="bodyContent">' +
-        `<p>State: ON</p>` +
-        `<p>LAT: ${position.lat}</p>` +
-        `<p>LAT: ${position.lng}</p>` +
-        "(last time update 29/6/2021)</p>" +
-        "</div>" +
-        "</div>";
+            '<div id="siteNotice">' +
+            "</div>" +
+            `<h4 id="firstHeading">Sensor ${num}</h4>` +
+            '<div id="bodyContent">' +
+            `<p>State: ON</p>` +
+            `<p>LAT: ${position.lat}</p>` +
+            `<p>LAT: ${position.lng}</p>` +
+            "(last time update 29/6/2021)</p>" +
+            "</div>" +
+            "</div>";
         const infowindow = new google.maps.InfoWindow({
             content: contentString,
         });
-        // sensors.addListener("click", () => {
-        //     showAndUpdateData(num)
-        //     infowindow.open({
-        //         anchor: sensors,
-        //         map
-        //     });
-        // });
+        sensors.addListener("click", () => {
+            showAndUpdateData(num)
+            infowindow.open({
+                anchor: sensors,
+                map
+            });
+        });
     }
+
+    function showAndUpdateData(sensor_num){
+        var info_sensor = $('.data-sensor')
+        $('.data-sensor .pos-1').text(`Sensor number ${sensor_num}`)
+        subscribe_data(sensor_num)
+        .then((res)=>{
+            $('#key1').text(`Dust: ${res.Dust1[0][1]}`)
+            $('#key2').text(`Humidity: ${res.Humidity1[0][1]}`)
+            $('#key3').text(`Pressure: ${res.Pressure1[0][1]}`)
+            $('#key4').text(`Temperature: ${res.Temperature1[0][1]}`)    
+            $('#key5').text(`PoF: ${res.PoF[0][1]}%`)
+            $('#key6').text(`CoF: ${res.CoF[0][1]}`)
+            $('#key7').text(`inspection_plan: ${res.inspection_plan[0][1]}`)
+        })
+        info_sensor.show()
+    }
+
+    window.initialize = initialize;
 }
--- src/clients/subscribe_data.client.js
+++ src/clients/subscribe_data.client.js
@@ -1,19 +1,36 @@
-function subscribe_data(entityId) {
+function subscribe_data(sensor_num) {
+    var entityId;
+    switch (sensor_num) {
+        case 0:
+            entityId = "23f06860-dd67-11eb-bb75-a1672e109977"
+            break
+        case 1:
+            entityId = "21905a70-de3a-11eb-bb75-a1672e109977"
+            break
+        case 2:
+            entityId = "64f8f9c0-de3a-11eb-bb75-a1672e109977"
+            break
+        case 3:
+            entityId = "b5688f10-de3a-11eb-bb75-a1672e109977"
+            break
+        default:
+            entityId = "23f06860-dd67-11eb-bb75-a1672e109977"
+    }
     return new Promise((resolve, reject) => {
-        var token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsaW5oLm5uMjgwMzk5QGdtYWlsLmNvbSIsInNjb3BlcyI6WyJURU5BTlRfQURNSU4iXSwidXNlcklkIjoiYmJhYTFlNTAtZDY1My0xMWViLTkzODEtYWIyYTFhOGRhYWYwIiwiZmlyc3ROYW1lIjoiTmd1eWVuIiwibGFzdE5hbWUiOiJOaGF0IExpbmgiLCJlbmFibGVkIjp0cnVlLCJwcml2YWN5UG9saWN5QWNjZXB0ZWQiOnRydWUsImlzUHVibGljIjpmYWxzZSwidGVuYW50SWQiOiJiYTgyOGU0MC1kNjUzLTExZWItOTM4MS1hYjJhMWE4ZGFhZjAiLCJjdXN0b21lcklkIjoiMTM4MTQwMDAtMWRkMi0xMWIyLTgwODAtODA4MDgwODA4MDgwIiwiaXNzIjoidGhpbmdzYm9hcmQuaW8iLCJpYXQiOjE2MjY2ODM2OTMsImV4cCI6MTYyODQ4MzY5M30.xZ9pfUfUajQ4h7TUhMeTbX6z0shas91epHGp-hVMTDoRYjITybPr-brRp9WM2h8ddEHNUDupRziF4tBaSge4Aw";
+        var token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsaW5oLm5uMjgwMzk5QGdtYWlsLmNvbSIsInNjb3BlcyI6WyJURU5BTlRfQURNSU4iXSwidXNlcklkIjoiYmJhYTFlNTAtZDY1My0xMWViLTkzODEtYWIyYTFhOGRhYWYwIiwiZmlyc3ROYW1lIjoiTmd1eWVuIiwibGFzdE5hbWUiOiJOaGF0IExpbmgiLCJlbmFibGVkIjp0cnVlLCJwcml2YWN5UG9saWN5QWNjZXB0ZWQiOnRydWUsImlzUHVibGljIjpmYWxzZSwidGVuYW50SWQiOiJiYTgyOGU0MC1kNjUzLTExZWItOTM4MS1hYjJhMWE4ZGFhZjAiLCJjdXN0b21lcklkIjoiMTM4MTQwMDAtMWRkMi0xMWIyLTgwODAtODA4MDgwODA4MDgwIiwiaXNzIjoidGhpbmdzYm9hcmQuaW8iLCJpYXQiOjE2Mjg1MjE1OTksImV4cCI6MTYzMDMyMTU5OX0.MrabIj_IPQ79wvlZzX31bREesWwitWU_vpmPg8meW9Qt-VrFdhvifMkCzcYOyzCNx_x1Dz7H6g4mWt3z1QMamQ";
         var webSocket = new WebSocket("wss://demo.thingsboard.io/api/ws/plugins/telemetry?token=" + token);
         webSocket.onopen = function () {
-        var object = {
-            tsSubCmds: [
-                {
-                    entityType: "DEVICE",
-                    entityId: entityId,
-                    scope: "LATEST_TELEMETRY",
-                    cmdId: 10
-                }
-            ],
-            historyCmds: [],
-            attrSubCmds: []
+            var object = {
+                tsSubCmds: [
+                    {
+                        entityType: "DEVICE",
+                        entityId: entityId,
+                        scope: "LATEST_TELEMETRY",
+                        cmdId: 10
+                    }
+                ],
+                historyCmds: [],
+                attrSubCmds: []
             };
             var data = JSON.stringify(object);
             webSocket.send(data);
@@ -24,7 +41,7 @@ function subscribe_data(entityId) {
             received_msg = JSON.parse(received_msg)
             received_msg = received_msg.data
             resolve(received_msg)
-            };
+        };
         webSocket.onclose = function (event) {
             console.log("Connection is closed! Please reload...");
         };
