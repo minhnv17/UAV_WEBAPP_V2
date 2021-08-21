d28b4ee2c51f374bb89bf29aea542c4c556f0636
minh1622 <huytranngoc098@gmail.com>
2021-08-21 14:03:56 +0700

    modify api

--- src/controllers/mqtt.controller.js
+++ src/controllers/mqtt.controller.js
@@ -4,7 +4,7 @@ exports.publish = (req, res) => {
     var data = req.body
     data = JSON.stringify(data)
     var client  = mqtt.connect('mqtt://demo.thingsboard.io',{
-        username: "Eqxw8KF94gKjZnIWqkwI"
+        username: "WHLwNcC2wBhHqfGnYiOV"
     })
     client.on('connect', function () {
         client.publish('v1/devices/me/telemetry', data)
